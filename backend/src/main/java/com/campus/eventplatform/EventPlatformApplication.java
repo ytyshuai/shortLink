c73d3e8f530873efc432bd093a52d78800111016
m0_61043641 <2721682889@qq.com>
2026-04-01 18:28:11 +0800

    fix: restore backend files deleted by mistake

---
+++ backend/src/main/java/com/campus/eventplatform/EventPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.campus.eventplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/campus/eventplatform/EventPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.eventplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
